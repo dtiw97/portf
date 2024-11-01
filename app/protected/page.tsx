85ae4962816306dc2423a063df5bc640fc060c63
tiw <dtiw.dummy@gmail.com>
2024-11-01 08:31:15 +0800

    mobile view friendly

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -25,13 +25,22 @@ export default async function ProtectedPage() {
       </div>
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
+        <pre className="text-xs font-mono p-3 rounded border max-h-32 max-w-full overflow-auto">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+        <pre className="text-xs font-mono p-3 rounded border max-h-32 max-w-full overflow-auto">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+        <pre className="text-xs font-mono p-3 rounded border max-h-32 max-w-full overflow-auto">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+        <pre className="text-xs font-mono p-3 rounded border max-h-32 max-w-full overflow-auto">
           {JSON.stringify(user, null, 2)}
         </pre>
       </div>
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
+        {/* <FetchDataSteps /> */}
       </div>
     </div>
   );
